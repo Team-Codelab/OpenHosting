b337ad4e9cda63587dcc58eaa6ca174f1028f8ed
pyrosine <pyrosine@gmail.com>
2012-04-02 20:06:13 +0100

    0038

--- includes/user.class.php
+++ includes/user.class.php
@@ -1,7 +1,16 @@
 <?php 
 
 class user{
-	function login($user,$pass){
+	function admin_auth($user,$auth){
+		$cfg=parse_ini_file("cfg.ini");
+		mysql_connect($cfg[mysql][server],$cfg[mysql][user],$cfg[mysql][pass]);
+		mysql_select_db($cfg[mysql][database]);
+		$query=mysql_query("select * from admins where user='".mysql_real_escape_string($user)."'");
+		if(mysql_num_rows($query)==0) return false;
+		if($auth!=mysql_result($query,0,auth)) return false;
+		else return true;
+	}
+	function user_login($user,$pass){
 		$pass_enc=sha1(mysql_real_escape_string($pass)."pyros");
 		$cfg=parse_ini_file("cfg.ini");
 		mysql_connect($cfg[mysql][server],$cfg[mysql][user],$cfg[mysql][pass]);
