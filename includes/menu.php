3dfa5be805339d951e3001d0510c36b865739807
pyrosine <pyrosine@gmail.com>
2012-04-02 18:41:31 +0100

    0026

--- includes/menu.php
+++ includes/menu.php
@@ -2,22 +2,22 @@
 <div style='width: 80%; height: 50px; background-color: #67C8FF; padding: 5px; margin-right: auto; margin-left: auto;'>
 	<div style='float: left;'>
 		<div id='menu'>
-			<a href='index.php' id='menu'>Home</a>
+			<a href='index.php' class='menu'>Home</a>
 		</div>
 		<div id='menu'>
-			<a href='hosting.php' id='menu'>Hosting</a>
+			<a href='hosting.php' class='menu'>Hosting</a>
 		</div>
 		<div id='menu'>	
-			<a href='domains.php' id='menu'>Domains</a>
+			<a href='domains.php' class='menu'>Domains</a>
 		</div>
 		<div id='menu'></div>
 	</div>
 	<div style='float: right;'>
 		<div id='menu'>
-			<a href='manager/' id='menu'>Login</a>
+			<a href='manager/' class='menu'>Login</a>
 		</div>
 		<div id='menu'>
-			<a href='support/' id='menu'>Support</a>
+			<a href='support/' class='menu'>Support</a>
 		</div>
 		<div id='menu'></div>
 	</div>
