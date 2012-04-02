b414a2500019409f78997bd986cd6fad4e65c76e
pyrosine <pyrosine@gmail.com>
2012-04-02 16:11:33 +0100

    0005

--- includes/menu.php
+++ includes/menu.php
@@ -1,11 +1,15 @@
 <div style='width: 100%; height: 50px; background-color: #00B2EE;'></div>
 <hr>
 <div style='width: 100%; height: 50px; background-color: #67C8FF;'>
-		<div style='width: 5px; border-right: dotted 1px'></div>
-	<a href='hosting.php'>Hosting</a>
-		<div style='width: 5px; border-right: dotted 1px'></div>	
-	<a href='hosting.php'>Domains</a>
-		<div style='width: 5px; border-right: dotted 1px'></div>
-	<a href='hosting.php'>Manager</a>
-		<div style='width: 5px; border-right: dotted 1px'></div>
+	<div style='float: left;'>
+			<div style='width: 5px; border-right: dotted 1px'></div>
+		<a href='hosting.php'>Hosting</a>
+			<div style='width: 5px; border-right: dotted 1px'></div>	
+		<a href='hosting.php'>Domains</a>
+			<div style='width: 5px; border-right: dotted 1px'></div>
+	</div>
+	<div style='float: right;'>
+		<a href='hosting.php'>Manager</a>
+			<div style='width: 5px; border-right: dotted 1px'></div>
+	</div>
 </div>
