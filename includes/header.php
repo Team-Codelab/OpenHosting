f764e110f53e01366d1cc132b60aa3964978f83d
pyrosine <pyrosine@gmail.com>
2012-04-02 16:59:49 +0100

    0008

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@ echo("<title>$title :: Zarkov Hosting</title>");
 echo("<style>
 	body{font-size: 14; font-family: courier;background:#FFFFFF;}
 	a{text-decoration: none; color: #EC3D00;}
+	.header_link{font-size:30px; font-type:bold;}
 </style>");
 echo("</head><body>");
 include("menu.php");
