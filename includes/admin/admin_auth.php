a563443599a04f473dc437890d3a95265f34d8a8
pyrosine <pyrosine@gmail.com>
2012-04-02 21:33:45 +0100

    0043f

--- includes/admin/admin_auth.php
+++ includes/admin/admin_auth.php
@@ -17,7 +17,8 @@ function print_login(){
 	$nomenu=1; include("../includes/header.php");
 	echo("<div style='width:100%;height:100%;'>
 		<div style='margin-right: auto; margin-left: auto; border: 1px dotted; width:300px; height:3000px; top: 50%;'>
-			<div style='margin-right: auto; margin-left: auto; width: 200px; height: 100px; bottom: 10%;'>
+			<div><img src='../../images/banner.jpg' width=256 height=64></div>
+			<div style='margin-right: auto; margin-left: auto; width: 200px; height: 100px;'>
 				<h3>Admin Logon</h3><form method='post' action='scripts/login.php'>
 				Username: <input type='text' name='admin_user'><br>
 				Password: <input type='password' name='admin_pass'><p>
