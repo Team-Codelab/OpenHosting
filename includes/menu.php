d7887759778cb732df0cb61ea1f8d055262d1507
pyrosine <pyrosine@gmail.com>
2012-04-02 18:56:39 +0100

    0032

--- includes/menu.php
+++ includes/menu.php
@@ -6,7 +6,7 @@
 <div style='width: 80%; height: 50px; background-color: #67C8FF; padding: 5px; margin-right: auto; margin-left: auto;'>
 	<div style='float: left;'>
 		<div id='menu'>
-			<a href='index.php' class='menu' <?php link_hover($id); $id++;?>>Home</a>
+			<a href='index.php' class='menu' id='<?php echo($id); ?>' <?php link_hover($id); $id++;?>>Home</a>
 		</div>
 		<div id='menu'>
 			<a href='hosting.php' class='menu'>Hosting</a>
