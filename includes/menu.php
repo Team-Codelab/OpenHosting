c5aef78d766239a84b5b63a38f2c57f30c8f3ded
pyrosine <pyrosine@gmail.com>
2012-04-02 16:43:22 +0100

    0005

--- includes/menu.php
+++ includes/menu.php
@@ -1,11 +1,11 @@
 <div style='width: 100%; height: 50px; background-color: #00B2EE;'></div>
 <hr>
-<div style='width: 100%; height: 50px; background-color: #67C8FF;'>
+<div style='width: 100%; height: 50px; background-color: #67C8FF; vertical-align: middle;'>
 	<div style='float: left;'>
-		<div style='border-right: dotted 1px; border-left: dotted 1px; float: left;'>
+		<div style='height: 30px; border-right: dotted 1px; border-left: dotted 1px; float: left;'>
 			<a href='hosting.php'>Hosting</a>
 		</div>
-		<div style='border-right: dotted 1px; border-left: dotted 1px; float: left;'>	
+		<div style='height: 30px; border-right: dotted 1px; border-left: dotted 1px; float: left;'>	
 			<a href='hosting.php'>Domains</a>
 		</div>
 	</div>
