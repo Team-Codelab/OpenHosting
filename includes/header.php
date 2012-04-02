864c189f495cce328682fb57eafc28f156a970c4
pyrosine <pyrosine@gmail.com>
2012-04-02 17:06:16 +0100

    0008

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,8 @@ echo("<title>$title :: Zarkov Hosting</title>");
 echo("<style>
 	body{font-size: 14; font-family: courier;background:#FFFFFF;}
 	a{text-decoration: none; color: #EC3D00;}
-	.header_link{font-size:30px; font-type:bold;}
+	a#menu{font-size:30px; font-type:bold;}
+	div#menu{height: 30px; padding: 5px; border-left: solid 2px; border-right: solid 1px; float: left;}
 </style>");
 echo("</head><body>");
 include("menu.php");
--- includes/menu.php
+++ includes/menu.php
@@ -2,14 +2,14 @@
 <hr>
 <div style='width: 100%; height: 50px; background-color: #67C8FF;'>
 	<div style='float: left;'>
-		<div style='height: 30px; padding: 5px; border-left: solid 2px; border-right: solid 1px; float: left;'>
-			<a href='hosting.php' class='header_link'>Hosting</a>
+		<div id='menu'>
+			<a href='hosting.php' id='menu'>Hosting</a>
 		</div>
-		<div style='height: 30px; padding: 5px; border-left: solid 1px; border-right: solid 1px; float: left;'>	
-			<a href='domains.php' class='header_link'>Domains</a>
+		<div id='menu'>	
+			<a href='domains.php' id='menu'>Domains</a>
 		</div>
 	</div>
 	<div style='float: right;'>
-		<a href='manager/' class='header_link'>Login</a>
+		<a href='manager/' id='menu'>Login</a>
 	</div>
 </div>
