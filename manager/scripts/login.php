c5781f54271fe593dcf4091d162ea1ac3b1d6ef3
pyrosine <pyrosine@gmail.com>
2012-04-02 21:41:42 +0100

    0043i

--- manager/scripts/login.php
+++ manager/scripts/login.php
@@ -1,16 +1,16 @@
 <?php
 
-$user=$_POST['admin_user'];
-$pass=$_POST['admin_pass'];
-if($_COOKIE['admin_user']!=null) header("location:index.php");
+$user=$_POST['user'];
+$pass=$_POST['pass'];
+if($_COOKIE['user']!=null) header("location:index.php");
 elseif($user=="" || $pass=="") header("location:index.php");
 
 include("../../includes/user.class.php");
 $admin = new user();
 if($admin->admin_login($user, $pass)==false) header("location:index.php?E=1");
 else{
-	setcookie('admin_user',$user,'','/','hosting.zarkov.net');
-	setcookie('admin_user',$user,'','/','hosting.zarkov.net');
+	setcookie('user',$user,'','/','hosting.zarkov.net');
+	setcookie('user',$user,'','/','hosting.zarkov.net');
 	header($_SERVER['HTTP_REFERER']);
 }
 
