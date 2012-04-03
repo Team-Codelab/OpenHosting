a136ca86e594798480146c2d03fa74498f39f4ff
pyrosine <pyrosine@gmail.com>
2012-04-03 14:21:45 +0100

    0053f

--- setup/scripts/core.php
+++ setup/scripts/core.php
@@ -6,7 +6,7 @@ include("../../includes/log.class.php");
 $log=new log();
 
 $cfg->add(core, title, $_POST['title']);
-$log->admin_action(0, 'Set core value: title: '.$_POST['title']);
+$log->admin_action(1, 'Set core value: title: '.$_POST['title']);
 
 $status=fopen("../status.php",'w');
 fwrite($status,'<?php $status=3 ?>');
