17a1bb28227a7e9da59ee81fdaf507a596cc1d66
pyrosine <pyrosine@gmail.com>
2012-04-03 13:17:15 +0100

    0051s

--- includes/log.class.php
+++ includes/log.class.php
@@ -25,7 +25,7 @@ class log {
 	}
 	function admin_action($uid,$action){
 		include(log_home()."/includes/mysql.init.php");
-		$query=mysql_query("select id from log_ip where ip='".$_SERVER['REMOTE_ADDR']."'");
+		$query=mysql_query("select id from log_ips where ip='".$_SERVER['REMOTE_ADDR']."'");
 		mysql_query("insert into log_admin_actions (uid,iid,action) values (".mysql_real_escape_string($uid).",".mysql_result($query,0,id).",'".mysql_real_escape_string($action)."')");
 	}
 }
