0b80c1cac3a2552c4b9956d48dd6699e6ec164eb
pyrosine <pyrosine@gmail.com>
2012-04-03 14:29:21 +0100

    0053j

--- includes/error.php
+++ includes/error.php
@@ -3,8 +3,8 @@
 if($_GET["E"]>=1) error($_GET["E"]);
 
 function error($n){
-	$errors[1]=="Bad login";
-	$errors[2]=="";
+	$errors[1]="Bad login";
+	$errors[2]="";
 	
 	echo("<div style='width:100%; height: 20px; top: 0px; background-color: red;' id='error'><font style='color: white; font-size: 15px;'>Error $n: ".$errors[$n]." <div style='float: right;'><a href='javascript: document.getElementById(".'"error"'.").style.display=".'"none"'.";'>X</a></font></div></div>");
 }
