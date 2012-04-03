62b6f09601677615d7ffa71101241348d30ab36f
pyrosine <pyrosine@gmail.com>
2012-04-03 14:30:39 +0100

    0053k

--- includes/error.php
+++ includes/error.php
@@ -6,7 +6,7 @@ function error($n){
 	$errors[1]="Bad login";
 	$errors[2]="";
 	
-	echo("<div style='width:100%; height: 20px; top: 0px; background-color: red;' id='error'><font style='color: white; font-size: 15px;'>Error $n: ".$errors[$n]." <div style='float: right;'><a href='javascript: document.getElementById(".'"error"'.").style.display=".'"none"'.";'>X</a></font></div></div>");
+	echo("<div style='width:100%; height: 30px; top: 0px; background-color: red;' id='error'><font style='color: white; font-size: 20px;'>Error #$n: ".$errors[$n]." <div style='float: right;'><a href='javascript: document.getElementById(".'"error"'.").style.display=".'"none"'.";'>X</a></font></div></div>");
 }
 
 
