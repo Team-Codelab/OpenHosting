8bc631e2a6098048314313f4a77a2c79354574bf
pyrosine <pyrosine@gmail.com>
2012-04-03 01:10:48 +0100

    0048n

--- includes/cfg_ini.class.php
+++ includes/cfg_ini.class.php
@@ -9,20 +9,17 @@ class ini {
 		$home="/home/site/main/~pyros/OpenHosting";
 		
 		$config=parse_ini_file("$home/includes/cfg.ini",true);
-		array_push($config[$header],array($key,$value));
+		array_push($config[$header],array($key=>$value));
 				print_r($config);
 		$new="";
 		for($z=0;$z<count($config);$z++){
 			$new.="[".key($config)."]\n";
 			for($x=0;$x<count($config[key($config)]);$x++){
-
 				$new.= key($config[key($config)])." = '".$config[key($config)][key($config[key($config)])]."'\n";
-
 				next($config[key($config)]);
 			}
 			next($config);
 		}
-				echo $str;
 		$cfg=fopen("$home/includes/cfg.ini","w");
 		fwrite($cfg,$new);
 		fclose($cfg);
