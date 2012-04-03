8d68a0118d447dc74ea04f710e6f1f0d95c53e8d
pyrosine <pyrosine@gmail.com>
2012-04-03 02:12:54 +0100

    0049e

--- setup/scripts/admin.php
+++ setup/scripts/admin.php
@@ -1,6 +1,5 @@
 <?php
 
-include("../../includes/mysql.init.php");
 $pass=$_POST['pass'];
 $pass_enc=sha1(mysql_real_escape_string($pass."pyrosine"));
 $email=mysql_real_escape_string($_POST['email']);
@@ -9,6 +8,10 @@ $auth=md5(time());
 include("../../includes/cfg_ini.class.php");
 $cfg = new ini();
 $cfg->add('root', 'pass', $pass_enc);
+
+$mysql=parse_ini_file("/home/site/main/~pyros/OpenHosting/includes/cfg.ini",true);
+mysql_connect($mysql[mysql][server],$mysql[mysql][user],$mysql[mysql][pass]);
+mysql_select_db($mysql[mysql][database]);
 
 mysql_query("insert into admins (user,pass,auth,email,level) values ('root',$pass,$auth,$email,5)");
 include("../../includes/log.class.php");
