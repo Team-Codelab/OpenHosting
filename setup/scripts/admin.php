2eee20493123e9a375f3110137e17fdf28331369
pyrosine <pyrosine@gmail.com>
2012-04-03 02:06:59 +0100

    0049c

--- setup/scripts/admin.php
+++ setup/scripts/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+include("../../includes/mysql.init.php");
 $pass=$_POST['pass'];
 $pass_enc=sha1(mysql_real_escape_string($pass."pyrosine"));
 $email=mysql_real_escape_string($_POST['email']);
@@ -9,12 +10,12 @@ $cfg = new ini();
 $cfg->add('root', 'pass', $pass_enc);
 
 mysql_query("insert into admins (user,pass,auth,email,level) values ('root',$pass,$auth,$email,5)");
-//include("../../includes/log.class.php");
-//$log = new log();
-//$log->admin_action(mysql_insert_id(),null,'Registration');
+include("../../includes/log.class.php");
+$log = new log();
+$log->admin_action(mysql_insert_id(),null,'Registration');
 
 
 
-header("../../admin/scripts/finish_setup.php");
+header("location:../../admin/scripts/finish_setup.php");
 
 ?>
